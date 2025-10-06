db587c49d947df91a2076e055a825f0b828796bf
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-10-06 11:08:47 +0300

    Update EHMTX.h

--- components/ehmtxv2/EHMTX.h
+++ components/ehmtxv2/EHMTX.h
@@ -37,7 +37,7 @@ const uint8_t TEXTSCROLLSTART = 8;
 const uint8_t TEXTSTARTOFFSET = (32 - 8);
 
 const uint16_t POLLINGINTERVAL = 250;
-static const char *const EHMTX_VERSION = "2025.8.1";
+static const char *const EHMTX_VERSION = "2025.10.1";
 static const char *const TAG = "EHMTXv2";
 
 enum show_mode : uint8_t
